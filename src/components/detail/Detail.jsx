32c7e8dcb9d614575779031a2f65e0ab1f3a477e
satakshirawat25 <satakshirawat.25may@gmail.com>
2025-12-04 20:02:03 +0530

    initial code

---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Detail.css'
+
+const Detail = () => {
+  return (
+    <div className='detail'>
+      <h1>detail</h1>
+    </div>
+  )
+}
+
+export default Detail
