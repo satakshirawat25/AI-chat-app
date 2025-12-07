deb4c04d5d9cdeab28ee619062084c88228049c1
satakshirawat25 <satakshirawat.25may@gmail.com>
2025-12-07 18:39:21 +0530

    code updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import Chat from "./components/chat/Chat.jsx"
 import Detail from "./components/detail/Detail"
+import Login from "./components/login/Login.jsx"
+import Notification from "./components/notification/Notification.jsx"
 import List from "./list/List"
 
 const App = () => {
+
+  const user = false
   return (
     <div className='container'>
-      <List/>
+      {
+        user?(
+          <>
+          <List/>
       <Chat/>
       <Detail/>
+          </>
+        ):(
+          <Login/>
+        )
+      }
+      <Notification/>
+      
     </div>
   )
 }
